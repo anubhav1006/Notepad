29809002af77766139fa452854993eb36cbfa8b7
anubhav <aj1006.aj@gmail.com>
2019-10-29 23:03:12 +0530

    App starting now xD

--- src/main/java/controller/NotepadController.java
+++ src/main/java/controller/NotepadController.java
@@ -1,14 +1,23 @@
 package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import service.NotepadService;
+
+import javax.xml.ws.Response;
 
 @RestController
 public class NotepadController {
 
-    @Autowired
-    NotepadService notepadService;
+    /*@Autowired
+    NotepadService notepadService;*/
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(){
+        return "Welcome to Spring boot API!!!";
+    }
 
 }
